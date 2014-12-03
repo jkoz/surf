894ffdd3ae48b04213c5a94e872231409f965fad
jkoz <phuoctaitp@gmail.com>
2014-12-02 18:25:31 -0700

    vi-liked keybind|

--- config.def.h
+++ config.def.h
@@ -71,20 +71,21 @@ static Key keys[] = {
     { MODKEY,               GDK_minus,  zoom,       { .i = -1 } },
     { MODKEY,               GDK_plus,   zoom,       { .i = +1 } },
 
-    { MODKEY,               GDK_l,      navigate,   { .i = +1 } },
-    { MODKEY,               GDK_h,      navigate,   { .i = -1 } },
+    { MODKEY,               GDK_i,      navigate,   { .i = +1 } },
+    { MODKEY,               GDK_o,      navigate,   { .i = -1 } },
 
     { MODKEY,               GDK_j,      scroll_v,   { .i = +1 } },
     { MODKEY,               GDK_k,      scroll_v,   { .i = -1 } },
-    { MODKEY,               GDK_b,      scroll_v,   { .i = -10000 } },
+    { MODKEY,               GDK_u,      scroll_v,   { .i = -10000 } },
+    { MODKEY,               GDK_d,      scroll_v,   { .i = +10000 } },
     { MODKEY,               GDK_space,  scroll_v,   { .i = +10000 } },
-    { MODKEY,               GDK_i,      scroll_h,   { .i = +1 } },
-    { MODKEY,               GDK_u,      scroll_h,   { .i = -1 } },
+    { MODKEY,               GDK_l,      scroll_h,   { .i = +1 } },
+    { MODKEY,               GDK_h,      scroll_h,   { .i = -1 } },
 
     { 0,                    GDK_F11,    fullscreen, { 0 } },
     { 0,                    GDK_Escape, stop,       { 0 } },
-    { MODKEY,               GDK_o,      source,     { 0 } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_o,      inspector,  { 0 } },
+    { MODKEY,               GDK_s,      source,     { 0 } },
+    { MODKEY|GDK_SHIFT_MASK,GDK_s,      inspector,  { 0 } },
 
     { MODKEY,               GDK_g,      spawn,      SETPROP("_SURF_URI", "_SURF_GO") },
     { MODKEY,               GDK_f,      spawn,      SETPROP("_SURF_FIND", "_SURF_FIND") },
